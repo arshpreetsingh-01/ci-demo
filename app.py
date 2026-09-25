f2b293dde4cdb08d7f13bb3ca2b6265015092f63
Arshpreet Singh <sardararshpreet@gmail.com>
2026-09-25 21:21:05 +0530

    solve linting issues

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-def add(a,b):
-    return a+b
+def add(a ,b):
+    return a + b
 
 
+def subtract(a ,b):
+     return a - b
 
 
+result1 = add(a, b)
+result2 = subtract(a, b)
 
-def subtract(a,b): return a-b
-
-
-result1 = add(a,b)
-result2 = subtract(a,b)
+print(result1, result2)
